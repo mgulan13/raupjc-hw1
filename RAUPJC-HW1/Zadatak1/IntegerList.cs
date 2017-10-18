1d6d9b78068a3dec42c2c79916fe59d8ab2b9eda
Marko Gulan <32739380+mgulan13@users.noreply.github.com>
2017-10-18 22:24:30 +0200

    Problem #1 solved

---
+++ RAUPJC-HW1/Zadatak1/IntegerList.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace Zadatak1
+{
+    public class IntegerList : IIntegerList
+    {
+        private int[] _internalStorage;
+
+        private int _size;
+
+        public IntegerList() : this(4)
+        {
+        }
+
+        public IntegerList(int initialSize)
+        {
+            Count = 0;
+            _size = initialSize;
+            _internalStorage = new int[initialSize];
+        }
+
+        public int Count { get; private set; }
+
+        public void Add(int item)
+        {
+            if (Count == _size)
+            {
+                _size *= 2;
+
+                int[] tempArray = new int[_size];
+                _internalStorage.CopyTo(tempArray, 0);
+                _internalStorage = tempArray;
+            }
+
+            _internalStorage[Count++] = item;
+        }
+
+        public void Clear()
+        {
+            Count = 0;
+        }
+
+        public bool Contains(int item)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (_internalStorage[i] == item)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public int GetElement(int index)
+        {
+            if (index < 0 || index >= Count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            return _internalStorage[index];
+        }
+
+        public int IndexOf(int item)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (_internalStorage[i] == item)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public bool Remove(int item)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (_internalStorage[i] == item)
+                {
+                    return RemoveAt(i);
+                }
+            }
+
+            return false;
+        }
+
+        public bool RemoveAt(int index)
+        {
+            if (index < 0 || index > _internalStorage.Length)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            if (index > Count)
+            {
+                return false;
+            }
+
+            for (int i = index; i < Count - 1; i++)
+            {
+                _internalStorage[i] = _internalStorage[i + 1];
+            }
+            Count--;
+            return true;
+        }
+    }
+}
